b35ce5debfb952ae1a175df147bbf1327da216d3
salis <salisshuaibu11@gmail.com>
2021-09-06 10:54:03 +0000

    Implemented logout functionality in Nav.js

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,19 +1,35 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { logout } from "../../actions/userActions";
 
 import "./Nav.css";
 import { NavLink } from "react-router-dom";
 
 const Nav = () => {
-  return (
-    <nav className="nav">
-      <p className="navTitle">Product Store API</p>
-      <p className="logout">
-        <NavLink to="/logout">
-          <a>Logout</a>
-        </NavLink>
-      </p>
-    </nav>
-  );
+  const dispatch = useDispatch();
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
+
+  if (!userInfo) {
+    return null;
+  } else {
+    return (
+      <nav className="nav">
+        <p className="navTitle">Product Store API</p>
+        <p className="logout">
+          <NavLink to="/logout">
+            <a onClick={logoutHandler}>{!userInfo ? "Login" : "Logout"}</a>
+          </NavLink>
+        </p>
+      </nav>
+    );
+  }
 };
 
 export default Nav;
