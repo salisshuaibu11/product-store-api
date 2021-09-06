93adf8036c935931d8f6dfcc394071e9611c56ae
salis <salisshuaibu11@gmail.com>
2021-09-06 11:00:54 +0000

    Added Register Screen

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import Navbar from "./components/Nav/Nav";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
 
 const App = () => {
   return (
     <Router>
       <Navbar />
-      <Route path="/login" component={LoginScreen} />
+      <Route exact path="/" component={LoginScreen} />
+      <Route path="/register" component={RegisterScreen} />
     </Router>
   );
 };
